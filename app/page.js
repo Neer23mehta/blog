431d0e620140f21cbafbbf7f4c0ca07c4d4a17a3
Drashti Shiyani <drashti.shiyani@tristatetechnology.co.in>
2025-03-26 16:23:53 +0530

    first

---
+++ app/page.js
@@ -0,0 +1,12 @@
+'use client'
+import { Blogitems } from "@/components/Blogitems";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Blogitems/>
+    </>
+  );
+}
